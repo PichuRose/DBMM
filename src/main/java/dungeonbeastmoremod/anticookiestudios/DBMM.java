468da4eaa3c09f0355eaf34b066a97fd756e698a
PichuRose <gholson.brodie@gmail.com>
2021-11-13 13:11:43 -0600

    Initial commit

---
+++ src/main/java/dungeonbeastmoremod/anticookiestudios/DBMM.java
@@ -0,0 +1,10 @@
+package dungeonbeastmoremod.anticookiestudios;
+
+import net.fabricmc.api.ModInitializer;
+
+public class DBMM implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/dungeonbeastmoremod/anticookiestudios/client/DBMMClient.java
@@ -0,0 +1,11 @@
+package dungeonbeastmoremod.anticookiestudios.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+@net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
+public class DBMMClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
